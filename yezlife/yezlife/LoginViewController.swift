f26b8248ca2f4b17a391e97ef95dcfdfd7465df9
AndrewYeo02 <andrewyeohao02@gmail.com>
2022-03-19 12:53:34 +0000

    Create Login and Signup Page

--- yezlife/yezlife/LoginViewController.swift
+++ yezlife/yezlife/LoginViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class LoginViewController: UIViewController {
 
@@ -13,17 +15,19 @@ class LoginViewController: UIViewController {
     @IBOutlet var email: UITextField!
     @IBOutlet var password: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
-
+    //override func viewDidAppear(_ animated:Bool){
+       // checkUserInfo()
+   // }
     
     
     @IBAction func loginTab(_ sender: Any) {
+        validateFields()
     }
     
     @IBAction func createAccTab(_ sender: Any) {
@@ -31,10 +35,36 @@ class LoginViewController: UIViewController {
         let vc = storyboard.instantiateViewController(identifier: "signUp")
         vc.modalPresentationStyle = .overFullScreen
         present(vc,animated:true)
+    }
+    func validateFields(){
+        if email.text?.isEmpty == true {
+            print("No Email Text")
+            return
+        }
+        if password.text?.isEmpty == true{
+            print("No Password Text")
+            return
+        }
         
-        
-        
-        
-        
+        login()
+    }
+    func login(){
+        Auth.auth().signIn(withEmail: email.text!, password:password.text!){ [weak self] authResult , err in
+            guard let strongSelf = self else {return}
+            if let err = err {
+                print(err.localizedDescription)
+                
+            }
+            self!.checkUserInfo()
+        }
+    }
+    func checkUserInfo(){
+        if Auth.auth().currentUser != nil{
+            print(Auth.auth().currentUser?.uid)
+            let storyboard = UIStoryboard(name : "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(identifier: "mainHome")
+            vc.modalPresentationStyle = .overFullScreen
+            present(vc,animated:true)
+        }
     }
 }
